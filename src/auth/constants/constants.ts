8a0169b9452b3b5da2fc8422159cc008a452bd0a
Bruna Melo <bmelo.silva25@gmail.com>
2026-01-30 07:59:03 -0300

    implementação dos testes

--- src/auth/constants/constants.ts
+++ src/auth/constants/constants.ts
@@ -1,4 +1,3 @@
 export const jwtConstants = {
-    secret: 'e9375ee6afb88ff9c53326da905c2f369de6778cad778206971f1fe08194f687',
+  secret: 'e9375ee6afb88ff9c53326da905c2f369de6778cad778206971f1fe08194f687',
 };
-
